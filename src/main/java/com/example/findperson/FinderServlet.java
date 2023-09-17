7c4525ec054e7bf4c0936cebbe03ae7e4f7418cd
ljl2107 <3234348964@qq.com>
2023-09-17 21:22:53 +0800

    由于不想动web.xml的代码，所以索性抛弃了@的注释方式

--- src/main/java/com/example/findperson/FinderServlet.java
+++ src/main/java/com/example/findperson/FinderServlet.java
@@ -25,100 +25,100 @@ import java.util.Map;
  * @Date: 2023/9/16
  * @Description: com.example.findperson
  */
-@WebServlet(name = "finder", value = "/find")
+//@WebServlet(name = "finder", value = "/find")
 public class FinderServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     // contact table
     private List<Map<String, Object>> contacts = new ArrayList<Map<String, Object>>();
 
-//    public void init() throws ServletException {
-//        try {
-//            String files = getInitParameter("contacts");
-//
-//            files = files.trim();
-//            files = files.replace(',', ',');
-//            String[] file_name_array = files.split(",");
-//
-//            for (int i = 0; i < file_name_array.length; i++) {
-//                String file_name = file_name_array[i];
-//                file_name = file_name.trim();
-//                if (file_name.length() == 0) {
-//                    continue;
-//                }
-//
-//                File file = new File(getServletContext().getRealPath("/WEB-INF/contacts/" + file_name));
-//
-//                FileInputStream fis = new FileInputStream(file);
-//
-//                Workbook book = null;
-//                try {
-//                    book = new XSSFWorkbook(fis);
-//                } catch (Exception ex) {
-//                    book = new HSSFWorkbook(fis);
-//                }
-//
-//                Sheet sheetAt = book.getSheetAt(0);
-//
-//                for (Row row : sheetAt) {
-//                    if (row.getRowNum() == 0) {
-//                        continue;
-//                    }
-//
-//                    if (row == null) {
-//                        break;
-//                    }
-//
-//                    Cell cell = row.getCell(0);
-//
-//                    if (cell == null) {
-//                        break;
-//                    }
-//
-//                    double no = row.getCell(0).getNumericCellValue();
-//                    String id = row.getCell(1).getStringCellValue();
-//                    String name = row.getCell(2).getStringCellValue();
-//                    String strClass = "";
-//                    String mobile = "";
-//                    String email = "";
-//
-//                    cell = row.getCell(3);
-//                    if (cell != null) {
-//                        strClass = cell.getStringCellValue();
-//                    }
-//
-//                    cell = row.getCell(4);
-//                    if (cell != null) {
-//                        cell.setCellType(CellType.STRING);
-//                        mobile = cell.getStringCellValue();
-//                    }
-//
-//                    cell = row.getCell(5);
-//                    if (cell != null) {
-//                        cell.setCellType(CellType.STRING);
-//                        email = cell.getStringCellValue();
-//                    }
-//
-//                    Map<String, Object> record = new HashMap<String, Object>();
-//                    record.put("id", id);
-//                    record.put("name", name);
-//                    record.put("gender", null);
-//                    record.put("class", strClass);
-//                    record.put("mobile", mobile);
-//                    record.put("email", email);
-//
-//                    contacts.add(record);
-//
-//                }
-//
-//                book.close();
-//                fis.close();
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void init() throws ServletException {
+        try {
+            String files = getInitParameter("contacts");
+
+            files = files.trim();
+            files = files.replace(',', ',');
+            String[] file_name_array = files.split(",");
+
+            for (int i = 0; i < file_name_array.length; i++) {
+                String file_name = file_name_array[i];
+                file_name = file_name.trim();
+                if (file_name.length() == 0) {
+                    continue;
+                }
+
+                File file = new File(getServletContext().getRealPath("/WEB-INF/contacts/" + file_name));
+
+                FileInputStream fis = new FileInputStream(file);
+
+                Workbook book = null;
+                try {
+                    book = new XSSFWorkbook(fis);
+                } catch (Exception ex) {
+                    book = new HSSFWorkbook(fis);
+                }
+
+                Sheet sheetAt = book.getSheetAt(0);
+
+                for (Row row : sheetAt) {
+                    if (row.getRowNum() == 0) {
+                        continue;
+                    }
+
+                    if (row == null) {
+                        break;
+                    }
+
+                    Cell cell = row.getCell(0);
+
+                    if (cell == null) {
+                        break;
+                    }
+
+                    double no = row.getCell(0).getNumericCellValue();
+                    String id = row.getCell(1).getStringCellValue();
+                    String name = row.getCell(2).getStringCellValue();
+                    String strClass = "";
+                    String mobile = "";
+                    String email = "";
+
+                    cell = row.getCell(3);
+                    if (cell != null) {
+                        strClass = cell.getStringCellValue();
+                    }
+
+                    cell = row.getCell(4);
+                    if (cell != null) {
+                        cell.setCellType(CellType.STRING);
+                        mobile = cell.getStringCellValue();
+                    }
+
+                    cell = row.getCell(5);
+                    if (cell != null) {
+                        cell.setCellType(CellType.STRING);
+                        email = cell.getStringCellValue();
+                    }
+
+                    Map<String, Object> record = new HashMap<String, Object>();
+                    record.put("id", id);
+                    record.put("name", name);
+                    record.put("gender", null);
+                    record.put("class", strClass);
+                    record.put("mobile", mobile);
+                    record.put("email", email);
+
+                    contacts.add(record);
+
+                }
+
+                book.close();
+                fis.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
