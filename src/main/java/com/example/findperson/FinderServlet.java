95ff729fe2c0a62376f5799e7f259340256f9e12
ljl2107 <3234348964@qq.com>
2023-09-17 21:08:03 +0800

    成功实现前端页面向后端传输数据，可以去实现判断了。

--- src/main/java/com/example/findperson/FinderServlet.java
+++ src/main/java/com/example/findperson/FinderServlet.java
@@ -32,102 +32,108 @@ public class FinderServlet extends HttpServlet {
     // contact table
     private List<Map<String, Object>> contacts = new ArrayList<Map<String, Object>>();
 
-    public void init() throws ServletException {
-        try {
-            String files = getInitParameter("contacts");
-
-            files = files.trim();
-            files = files.replace(',', ',');
-            String[] file_name_array = files.split(",");
-
-            for (int i = 0; i < file_name_array.length; i++) {
-                String file_name = file_name_array[i];
-                file_name = file_name.trim();
-                if (file_name.length() == 0) {
-                    continue;
-                }
-
-                File file = new File(getServletContext().getRealPath("/WEB-INF/contacts/" + file_name));
-
-                FileInputStream fis = new FileInputStream(file);
-
-                Workbook book = null;
-                try {
-                    book = new XSSFWorkbook(fis);
-                } catch (Exception ex) {
-                    book = new HSSFWorkbook(fis);
-                }
-
-                Sheet sheetAt = book.getSheetAt(0);
-
-                for (Row row : sheetAt) {
-                    if (row.getRowNum() == 0) {
-                        continue;
-                    }
-
-                    if (row == null) {
-                        break;
-                    }
-
-                    Cell cell = row.getCell(0);
-
-                    if (cell == null) {
-                        break;
-                    }
-
-                    double no = row.getCell(0).getNumericCellValue();
-                    String id = row.getCell(1).getStringCellValue();
-                    String name = row.getCell(2).getStringCellValue();
-                    String strClass = "";
-                    String mobile = "";
-                    String email = "";
-
-                    cell = row.getCell(3);
-                    if (cell != null) {
-                        strClass = cell.getStringCellValue();
-                    }
-
-                    cell = row.getCell(4);
-                    if (cell != null) {
-                        cell.setCellType(CellType.STRING);
-                        mobile = cell.getStringCellValue();
-                    }
-
-                    cell = row.getCell(5);
-                    if (cell != null) {
-                        cell.setCellType(CellType.STRING);
-                        email = cell.getStringCellValue();
-                    }
-
-                    Map<String, Object> record = new HashMap<String, Object>();
-                    record.put("id", id);
-                    record.put("name", name);
-                    record.put("gender", null);
-                    record.put("class", strClass);
-                    record.put("mobile", mobile);
-                    record.put("email", email);
-
-                    contacts.add(record);
-
-                }
-
-                book.close();
-                fis.close();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public void init() throws ServletException {
+//        try {
+//            String files = getInitParameter("contacts");
+//
+//            files = files.trim();
+//            files = files.replace(',', ',');
+//            String[] file_name_array = files.split(",");
+//
+//            for (int i = 0; i < file_name_array.length; i++) {
+//                String file_name = file_name_array[i];
+//                file_name = file_name.trim();
+//                if (file_name.length() == 0) {
+//                    continue;
+//                }
+//
+//                File file = new File(getServletContext().getRealPath("/WEB-INF/contacts/" + file_name));
+//
+//                FileInputStream fis = new FileInputStream(file);
+//
+//                Workbook book = null;
+//                try {
+//                    book = new XSSFWorkbook(fis);
+//                } catch (Exception ex) {
+//                    book = new HSSFWorkbook(fis);
+//                }
+//
+//                Sheet sheetAt = book.getSheetAt(0);
+//
+//                for (Row row : sheetAt) {
+//                    if (row.getRowNum() == 0) {
+//                        continue;
+//                    }
+//
+//                    if (row == null) {
+//                        break;
+//                    }
+//
+//                    Cell cell = row.getCell(0);
+//
+//                    if (cell == null) {
+//                        break;
+//                    }
+//
+//                    double no = row.getCell(0).getNumericCellValue();
+//                    String id = row.getCell(1).getStringCellValue();
+//                    String name = row.getCell(2).getStringCellValue();
+//                    String strClass = "";
+//                    String mobile = "";
+//                    String email = "";
+//
+//                    cell = row.getCell(3);
+//                    if (cell != null) {
+//                        strClass = cell.getStringCellValue();
+//                    }
+//
+//                    cell = row.getCell(4);
+//                    if (cell != null) {
+//                        cell.setCellType(CellType.STRING);
+//                        mobile = cell.getStringCellValue();
+//                    }
+//
+//                    cell = row.getCell(5);
+//                    if (cell != null) {
+//                        cell.setCellType(CellType.STRING);
+//                        email = cell.getStringCellValue();
+//                    }
+//
+//                    Map<String, Object> record = new HashMap<String, Object>();
+//                    record.put("id", id);
+//                    record.put("name", name);
+//                    record.put("gender", null);
+//                    record.put("class", strClass);
+//                    record.put("mobile", mobile);
+//                    record.put("email", email);
+//
+//                    contacts.add(record);
+//
+//                }
+//
+//                book.close();
+//                fis.close();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=UTF-8");
+        response.sendRedirect("hello.html");
+        System.out.println("参数列表"+request.getParameterNames());
+        System.out.println("参数值："+request.getParameter("queryparm"));
+        System.out.println("访问get");
+//        System.out.println(request.getParameterNames());
+//        response.sendRedirect("finder.html");
+    }
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + "Finder" + "</h1>");
-        out.println("</body></html>");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("访问到了Finder的post");
+        doGet(req, resp);
     }
 }
